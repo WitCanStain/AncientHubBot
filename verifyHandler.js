09611fd1cfb7d2f1ce6bedd71a3b65f86ec0c5a2
Oxblood <>
2025-03-17 14:04:44 +0200

    changed member fetching

--- verifyHandler.js
+++ verifyHandler.js
@@ -70,7 +70,7 @@ const acceptUserVerification = async (message) => {
         if (userid) {
             // const guild = ds_client.guilds.cache.get(process.env.GUILD_ID);
             // let member = message.guild.members.cache.get(userid);//guild.members.cache.get(userid);
-            let member = await ds_client.users.fetch(userid);
+            let member = await ds_client.users.fetch(String(userid));
             let role_to_add = member.guild.roles.cache.find(role => role.name.toLowerCase() === "ancient");
             let ancient_unverified_role_to_remove = member.guild.roles.cache.find(role => role.name.toLowerCase() === "ancient (unverified)");
             let unvetted_role_to_remove = member.guild.roles.cache.find(role => role.name.toLowerCase() === "unvetted");
